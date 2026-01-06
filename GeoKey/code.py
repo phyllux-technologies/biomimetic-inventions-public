c9b38768d6327bb98ba7e540c7e7e2db42c5bbae
phibronotchi-beep <phibronotchi@gmail.com>
2026-01-06 07:55:12 -0700

    Update code.py
    Edit - Name

--- GeoKey/code.py
+++ GeoKey/code.py
@@ -1,11 +1,11 @@
 import numpy as np
 import hashlib
 
-def geokey(N=121, seed=''):
+def phikey(N=121, seed=''):
     phi = (1 + np.sqrt(5)) / 2
     theta = np.arange(1, N+1) * (360 / phi) % 360
     r = np.sqrt(np.arange(1, N+1))
     points = str(r * np.cos(np.deg2rad(theta)) + r * np.sin(np.deg2rad(theta)))
     return hashlib.sha256((points + seed).encode()).hexdigest()
 
-print(geokey())
+print(phikey())
